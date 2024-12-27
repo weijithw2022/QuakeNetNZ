0c350e0daa32c474d9ceb8a46c67021358fe9301
weijithw2022 <weijithwimalasiri2020@gmail.com>
2024-12-27 18:55:00 +0530

    Added the last layer with softmax

--- src/unet.py
+++ src/unet.py
@@ -15,7 +15,19 @@ class conv1x1(nn.Module):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         return x
-
+    
+class lastconv1x1(nn.Module):
+    """
+        1x1 convolutional layer with and softmax Activation
+    """
+    def __init__(self, in_channels, out_channels, kernel_size=7, stride=1, padding=3):
+        super(lastconv1x1, self).__init__()
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, stride, padding)
+        
+    def forward(self, x):
+        x = F.softmax(self.conv(x), dim=1)
+        return x
+    
 class DownSampling(nn.Module):
     """
         Down Sampling with Convolution and Max Pooling
@@ -36,14 +48,14 @@ class UpSampling(nn.Module):
         Up Sampling with Convolution and Upsample
         Convolution + Stride + Relu Activation
     """
-    def __init__(self, in_channels, out_channels, kernel_size=7, stride=4, padding_down=0, padding_conv = 3):
+    def __init__(self, in_channels, out_channels, kernel_size=7, stride=4, padding_up=0, padding_conv = 3):
         super(UpSampling, self).__init__()
-        self.upsample = nn.ConvTranspose1d(in_channels, out_channels, kernel_size, stride, padding=padding_down)
+        self.upsample = nn.ConvTranspose1d(in_channels, out_channels, kernel_size, stride, padding=padding_up)
         self.conv = nn.Conv1d(out_channels*2, out_channels, kernel_size, stride =1, padding=padding_conv)
 
     def forward(self, x, skip_x):
         x = F.relu(self.upsample(x))
-        x = torch.cat(x, skip_x, dims= 1)
+        x = torch.cat(x, skip_x, dim= 1)
         x = F.relu(self.conv(x))
         return x
 
@@ -63,23 +75,24 @@ class uNet(nn.Module):
         self.up_channels = self.down_channels[::-1]
 
         self.down_layers = nn.ModuleList()
-        in_channels = self.in_channels
 
         # Down Sampling Layers
         for i in range(len(self.down_channels)):
-            ins = self.in_channels if i == 0 else outs
-            outs = base_filters
-            self.down_layers.append(DownSampling(in_channels=ins, out_channels=outs))
-            in_channels = out_channels
-            base_filters *= 2
-        
+            ins = self.in_channels if i == 0 else self.down_channels[i-1]
+            outs = self.down_channels[i]
+            if i==0:
+                self.down_layers.append(conv1x1(in_channels=ins, out_channels=outs))
+            else:
+                self.down_layers.append(DownSampling(in_channels=ins, out_channels=outs))
+
         self.up_layers = nn.ModuleList()
 
         # Up Sampling Layers
-        for i in range(depth-2):
-            ins = outs
-            outs = ins//2
+        for i in range(len(self.down_channels)-1):
+            ins = self.up_channels[i]
+            outs = self.up_channels[i+1]
             self.up_layers.append(UpSampling(in_channels=ins, out_channels=outs))
+        self.up_layers.append(lastconv1x1(in_channels=self.up_channels[-1], out_channels=self.out_channels))
     
     def forward(self,x):
         skip_connections = []
@@ -87,8 +100,11 @@ class uNet(nn.Module):
             x = layer(x)
             skip_connections.append(x)
         
-        for i, layer in enumerate(self.up_layers):
-            x = layer(x, skip_connections[-i-1])
+        for i, layer in enumerate(self.up_layers[:-1]):
+            x = layer(x, skip_connections[i+1])
+        
+        # Final Output Layer
+        x = self.up_layers[-1](x)
 
         return x
 
