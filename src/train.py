fee4ca5db5c69be598a9b6819fa8185b71f7b262
Weijith Wimalasiri <110900801+weijithw2022@users.noreply.github.com>
2025-02-18 09:19:47 +0530

    Update for training

--- src/train.py
+++ src/train.py
@@ -1,7 +1,7 @@
-
 from utils import *
 from database_op import *
 from config import Config, MODE_TYPE, MODEL_TYPE, NNCFG
+from unet import uNet
 
 def _train(model, dataloader, optimizer, criterion, epoch_iter=50):
 
@@ -11,16 +11,20 @@ def _train(model, dataloader, optimizer, criterion, epoch_iter=50):
       for batch_X, batch_y in dataloader:
          optimizer.zero_grad()
          output = model(batch_X)
-         loss = criterion(output.squeeze(), batch_y)
+         output = output.squeeze()
+         batch_y = batch_y.squeeze()
+         # output = torch.mean(output, dim=1)
+         # print(f'Output ; {output}')
+         loss = criterion(output, batch_y)
          loss.backward()
          optimizer.step()
          epoch_loss+= loss.item()
-      
       avg_epoch_loss = (epoch_loss/len(dataloader))
       train_losses.append(avg_epoch_loss)
       print(f'Epoch {epoch+1}, Loss: {avg_epoch_loss}')
 
    return model,train_losses
+   
 
 
 def train(cfg):
@@ -28,6 +32,7 @@ def train(cfg):
    nncfg = NNCFG()
    nncfg.argParser()
 
+   # print(cfg.TRAIN_DATA)
    hdf5_file = h5py.File(cfg.TRAIN_DATA, 'r')
    p_data, s_data, noise_data = getWaveData(cfg, hdf5_file)
    
@@ -36,12 +41,16 @@ def train(cfg):
    s_data = np.array(s_data)
    noise_data = np.array(noise_data)
 
+   # print(p_data)
+
    positive_data = np.concatenate((p_data , s_data))
 
    X = np.concatenate([positive_data, noise_data], axis=0)
    Y = np.array([1] * len(positive_data) + [0] * len(noise_data))  # 1 for P wave, 0 for noise
+   # print(Y)
+   # print(f"X shape: {X.shape}, Y shape: {Y.shape}")
 
-   dataset = TensorDataset(torch.tensor(X, dtype=torch.float32), torch.tensor(Y, dtype=torch.long))
+   dataset = TensorDataset(torch.tensor(X, dtype=torch.float32), torch.tensor(Y, dtype=torch.float))
    dataloader = DataLoader(dataset, batch_size=nncfg.batch_size, shuffle=True)
 
    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -61,6 +70,15 @@ def train(cfg):
       optimizer = torch.optim.Adam(model.parameters(), lr=nncfg.learning_rate)
       criterion = nn.BCEWithLogitsLoss()
       model, train_losses = _train(model, dataloader, optimizer, criterion, nncfg.epoch_count)
+   
+   elif cfg.MODEL_TYPE == MODEL_TYPE.UNET:
+      model = uNet(in_channels=3, out_channels = 1).to(device)
+      # As they have used cross-entropy in the paper, I am using the same
+      # criterion = nn.CrossEntropyLoss()
+      # criterion = nn.BCELoss()
+      criterion = nn.BCEWithLogitsLoss()
+      optimizer = torch.optim.Adam(model.parameters(), lr=nncfg.learning_rate)
+      model, train_losses = _train(model, dataloader, optimizer, criterion, epoch_iter=nncfg.epoch_count)
 
    
    cfg.MODEL_FILE_NAME = cfg.MODEL_PATH + model.model_id
